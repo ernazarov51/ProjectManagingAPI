1c730c4b1bdd39c0584446a71475ce4cecc01c17
ernazarov51 <ravshantursunboyev55@gmail.com>
2026-02-03 11:31:37 +0500

    add requirements

--- ProjectManagingAPI/settings.py
+++ ProjectManagingAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0!kc@l=wdjb2a$dtfj&%yum&yei(q=4v7g#gob0*xxx93a81h@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
